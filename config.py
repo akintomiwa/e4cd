84ff93bb5a27397112bea282a03689e2cde04632
Isaac Akin-Onigbinde <aakinonigbinde@gmail.com>
2022-12-07 22:34:59 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# Config
+ticks = 3600 # 3600 ticks = 3600 seconds = 1 hour
+no_customers = 500
+avg_service_time = 45 # ticks/seconds per customer
+gross_margin_per_customer = 10 # dollars
+cost_per_counter = 300 # dollars
