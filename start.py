e566e6e3deac83c52644c9b205e6fcc7a95bae55
Akintomiwa Akin-Onigbinde <aakinonigbinde@gmail.com>
2023-04-03 09:27:11 +0100

    mod: interface change

--- start.py
+++ start.py
@@ -1,32 +1,35 @@
 # imports
-import os
-import seaborn as sns
+# import os
+# import seaborn as sns
 from random import choice
 import warnings
 warnings.simplefilter("ignore")
-import pandas as pd
-import numpy as np
-import mesa
-from mesa import Agent, Model
-from mesa.time import RandomActivation, RandomActivationByType, SimultaneousActivation
+
+# from mesa import Agent, Model
+
 from mesa.datacollection import DataCollector
-from matplotlib import pyplot as plt, patches
-import scipy.stats as ss
+# from matplotlib import pyplot as plt, patches
+# import scipy.stats as ss
 import cufflinks as cf
 cf.go_offline()
-from plotly.offline import iplot
-from transitions import Machine
-import random
-from transitions.extensions import GraphMachine
-import graphviz
-import timeit
+# from plotly.offline import iplot
+# from transitions import Machine
+# import random
+# from transitions.extensions import GraphMachine
+# import graphviz
+# import timeit
+# import logging
+# from collections import Counter
+# import pandas as pd
+# import numpy as np
+# import mesa
+# from mesa.time import RandomActivation, RandomActivationByType, SimultaneousActivation
 from datetime import datetime
-import logging
-from collections import Counter
+
 
 
 import config.model_config as cfg
-import config.worker as worker
+# import config.worker as worker
 from EV.agent import EV, ChargeStation
 import EV.model as model
 from EV.statemachine import EVSM, LSM
@@ -36,14 +39,32 @@ This is the main file for the EV ABM simulator.
 It is used to run the model and collect the data.
 
 """
+# Parameterisation
+# from config file
+
+# today's date as string
+date_str = str(datetime.today())
+
 
 def run():
-    station_config = worker.read_csv(cfg.STATION_PATH +'stations.csv')
-    ticks = 48
-    no_evs = 2
-    model_run = model.EVModel(ticks=ticks, no_evs=no_evs, params=station_config)
-    for i in range(ticks):
+    model_run = model.EVModel(ticks=cfg.ticks, no_evs=cfg.no_evs, params=cfg.station_config)
+    for i in range(cfg.ticks):
         model_run.step()
+    return model_run
+
+# def extract_data(model):
+#     run_stats = model_run.datacollector.get_model_vars_dataframe()
+#     print(run_stats)
+
+# def export_data(model):
+#     run_stats = model_run.datacollector.get_model_vars_dataframe()
+#     # run_stats.to_csv(cfg.DATA_PATH + 'run_stats.csv', index=False)
+#     run_stats.to_excel(cfg.DATA_PATH + 'run_stats.xlsx', index=False)
+#     run_stats.to_csv(cfg.DATA_PATH + 'data_' + date_str[0:10] + '_' + str(cfg.no_evs) + '_EV_agent_model_output.csv')
+#     print('Data exported to csv and xlsx')
+
+
+
 
 
 if __name__ == '__main__':
